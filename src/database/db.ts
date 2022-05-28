c5a501043d733a65c8d5d9a3670fd1c5754bf2a8
Hasan Berkan Eroğlu <72980770+HBE18@users.noreply.github.com>
2022-05-28 22:57:19 +0300

    Last Point /no seat planning or ticket management

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,11 +1,8 @@
 import { Pool } from 'pg';
-import dbConfig from '../configs';
+import {dbConfig} from '../configs';
 
-const pool = new Pool(dbConfig);
+export const pool = new Pool(dbConfig);
 
-
-module.exports({
-    query : (text:string,params:Array<string>) => {
-        pool.query(text,params);
-    }
-});
+export const query = (text:string,params:Array<string>) => {
+    pool.query(text,params);
+}
